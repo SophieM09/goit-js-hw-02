76cb7aae512a151b2fe784b66a712047be168de6
Sofi <sofimin.93@gmail.com>
2025-06-13 19:04:30 -0700

    final

--- js/task-1.js
+++ js/task-1.js
@@ -2,8 +2,14 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const transactionTotal = quantity * pricePerDroid;
 
     if ( transactionTotal > customerCredits) {
-        return "Insufficient funds!"
+        return "Insufficient funds!";
     }
 
-    return `You ordered ${quantity} droids worth ${transactionTotal} credits!`
+    return `You ordered ${quantity} droids worth ${transactionTotal} credits!`;
 }
+
+ console.log(makeTransaction(5, 3000, 23000)); 
+ console.log(makeTransaction(3, 1000, 15000));
+ console.log(makeTransaction(10, 5000, 8000));
+ console.log(makeTransaction(8, 2000, 10000));
+ console.log(makeTransaction(10, 500, 5000));
